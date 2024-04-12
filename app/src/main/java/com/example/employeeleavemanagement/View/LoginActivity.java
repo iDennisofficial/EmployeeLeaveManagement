7b91988c8fb036be3cd5efc424b4fde91b50f205
iDennisofficial <dennisdamas100@gmail.com>
2024-04-12 16:58:15 +0300

    Added login activity xml file

--- app/src/main/java/com/example/employeeleavemanagement/View/LoginActivity.java
+++ app/src/main/java/com/example/employeeleavemanagement/View/LoginActivity.java
@@ -2,7 +2,7 @@ package com.example.employeeleavemanagement.View;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.window.SplashScreen;
+import androidx.core.splashscreen.SplashScreen;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +10,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 import com.example.employeeleavemanagement.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
 
-        
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashScreen.setKeepOnScreenCondition(new SplashScreen.KeepOnScreenCondition() {
+                    @Override
+                    public boolean shouldKeepOnScreen() {
+                        return false;
+                    }
+                });
+            }
+        },2000);
 
 
     }
